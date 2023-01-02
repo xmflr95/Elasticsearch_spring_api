1de37b993bc1734d13776973585c30cc9c592144
ChangHui Han <xmflr95@naver.com>
2023-01-02 14:21:03 +0900

    elastic log pilot

---
+++ src/main/java/com/log/logpilot/LogpilotApplication.java
@@ -0,0 +1,13 @@
+package com.log.logpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogpilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/log/logpilot/LogpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log.logpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
